c40822870c25085020a19dc8f1eff30cb79f221c
yuh <yuhwhu@foxmail.com>
2024-12-24 17:37:04 +0800

    update

--- GraphAgent-inference/run.sh
+++ GraphAgent-inference/run.sh
@@ -1,6 +1,6 @@
 export OPENAI_API_KEY="" # set your Planner API key here
 export SENTENCE_TRANSFORMER_MODEL_PATH="sentence-transformers/all-mpnet-base-v2"
-export GRAPH_ACTION_MODEL_PATH="GraphAgent/GraphAgent-7B"
+export GRAPH_ACTION_MODEL_PATH="GraphAgent/GraphAgent-8B"
 export GRAPH_TOKENIZE_MODEL_PATH="GraphAgent/GraphTokenizer"
 
 PLANNER="deepseek"
